db1bf64abadd1569e26bea855107ca3ab7fa7c69
Tor Magne <tor.magne@hougen.info>
2022-12-04 20:04:17 +0100

    Filter

--- delete-older-files.py
+++ delete-older-files.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 
-import sys, getopt, os, time
+import sys, getopt, os, time, fnmatch
 olderthan = '#days.has.to.be.defined'
 path = '#target.directory.has.to.be.defined'
-
+filter = '*'
 
 def usage():
     print('---------------------------------------------------')
-    print('Delete files older than')
+    print('Delete files older than...')
     print('---------------------------------------------------')
     print(sys.argv[0], 'needs input args')
-    print('-o --olderthan      Must be defined, days')
-    print('-d --dir            Must be defined, absolute (/)')
+    print('-o --olderthan      days, must be defined')
+    print('-d --dir            Must be defined absolute (/path)')
+    print('-f --filter         Dont need, default = "*"')
+    print('                    Obs! husk "" eksempel "NO*" ')
     sys.exit('---------------------------------------------------')
 
 # Block validate input args
 argi = sys.argv[1:]
 try:
-    opts, args = getopt.getopt(argi,"ht:o:d:", \
-    ["olderthan=","dir=","help="])
+    opts, args = getopt.getopt(argi,"ht:o:d:f:", \
+    ["olderthan=","dir=","filter=","help="])
 except getopt.GetoptError as err:
     print (err)
     usage()
@@ -29,13 +31,18 @@ for opt, arg in opts:
         olderthan = arg
     elif opt in ("-d", "--dir"):
         path = arg
+    elif opt in ("-f", "--filter"):
+        filter = arg
 if olderthan == 'path' \
 or path == '#target.directory.has.to.be.defined':
     usage()
 
+# Set filefilter and for each file, copy to target and remove source.
+files = fnmatch.filter(os.listdir(path), filter)
+if len(files) == 0:
+    sys.exit('No files found in ' + path + filter)
 now = time.time()
-
-for filename in os.listdir(path):
+for filename in files:
     if not os.path.isdir(os.path.join(path, filename)):
         filestamp = os.stat(os.path.join(path, filename)).st_mtime
         filecompare = now - int(olderthan) * 86400
